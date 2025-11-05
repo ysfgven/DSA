834cfe15925381643a972c060354711e25f8dc7a
ysfgven <yusuf.guven0@hotmail.com>
2025-11-06 00:19:59 +0300

    Added C++ ArrayStack implementation

---
+++ C++/Stack/ArrayStack.h
@@ -0,0 +1,55 @@
+#ifndef ARRAYSTACK_H
+#define ARRAYSTACK_H
+#include <iostream>
+
+
+
+template <class T>
+class ArrayStack {
+private:
+    int top;
+    int capacity;
+    T *elements;
+    public:
+    ArrayStack(int capacity) {
+        this->capacity = capacity;
+        this->elements = new T[capacity];
+        top = -1;
+    };
+
+    ~ArrayStack() {
+        delete[] elements;
+    };
+    bool isFull() const {
+        return top == capacity - 1;
+    };
+    bool isEmpty() const {
+        return top == -1;
+    };
+    bool push(T element) {
+        if (isFull()) {
+           std::cout << "Stack is full" << std::endl;
+           return false;
+        }
+        elements[++top] = element;
+        return true;
+
+    };
+    T pop() {
+        if (!isEmpty()) {
+           return elements[top--];
+        }
+        std::cout << "Stack is empty" << std::endl;
+        return T();//if its empty send default value.
+    };
+    T peek() const{
+        if (isEmpty()) {
+            std::cout << "Stack is empty" << std::endl;
+            return T();
+        }
+            return elements[top];
+        }
+    };
+
+
+#endif //ARRAYSTACK_H
